60a02be3e4c78044f170ba959e0550fdfc1919d1
yutsukimiyashita <j148015n@st.u-gakugei.ac.jp>
2018-09-16 15:38:06 +0900

    history

--- modeling/db/migrations/20180916062433-create_order_history.sql
+++ modeling/db/migrations/20180916062433-create_order_history.sql
@@ -1,4 +1,14 @@
 
 -- +migrate Up
+CREATE TABLE order_history (
+  id INT NOT NULL AUTO_INCREMENT,
+  user_id INT NOT NULL,
+  vegetable_id INT NOT NULL,
+  date DATE DEFAULT CURRENT_DATE ON UPDATE CURRENT_DATE,
+  PRIMARY KEY(id),
+  FOREIGN KEY(user_id) REFERENCES users(id),
+  FOREIGN KEY(vegetable_id) REFERENCES vegetables(id)
+);
+
 
 -- +migrate Down
