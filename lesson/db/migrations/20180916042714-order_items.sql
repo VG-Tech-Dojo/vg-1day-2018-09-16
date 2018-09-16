54cd1c44de38b35b4403f2b07c99bced2cfa9df5
yaya1026 <yaya.w.1026@gmail.com>
2018-09-16 13:46:07 +0900

    migration適用し直した

--- lesson/db/migrations/20180916042714-order_items.sql
+++ lesson/db/migrations/20180916042714-order_items.sql
@@ -1,4 +1,13 @@
 
 -- +migrate Up
-
+CREATE TABLE order_items (
+  id INT NOT NULL AUTO_INCREMENT,
+  order_id INT NOT NULL,
+  item_id INT NOT NULL,
+  amount INT NOT NULL,
+  PRIMARY KEY(id),
+  FOREIGN KEY(order_id) REFERENCES orders(id),
+  FOREIGN KEY(item_id) REFERENCES items(id)
+);
 -- +migrate Down
+DROP TABLE order_items;
