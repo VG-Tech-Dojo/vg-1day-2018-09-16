4553bed7cc76607954d83a3df0d092bb2aae3b43
clockvoid <hidenari.t1997nov@gmail.com>
2018-09-16 17:41:08 +0900

    fix: vegetable_point

--- modeling/db/migrations/20180916062416-create_vegetables.sql
+++ modeling/db/migrations/20180916062416-create_vegetables.sql
@@ -1,10 +1,9 @@
 
 -- +migrate Up
 CREATE TABLE vegetables (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    point INT NOT NULL,
-    PRIMARY KEY(id)
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  PRIMARY KEY(id)
 );
 
 -- +migrate Down
