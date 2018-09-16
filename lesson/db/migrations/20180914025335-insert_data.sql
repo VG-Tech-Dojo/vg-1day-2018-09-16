17e79f01949bbe889d620867fd8a8640222a2e47
pii5656 <microapple56@gmail.com>
2018-09-16 15:41:23 +0900

    misson2

--- lesson/db/migrations/20180914025335-insert_data.sql
+++ lesson/db/migrations/20180914025335-insert_data.sql
@@ -3,5 +3,7 @@ INSERT INTO users(email, name, address) VALUES
 ('dkkoma@example.com', 'dkkoma', '東京都目黒区ほげほげ'),
 ('pro_shunsuke@example.com', 'pro', '茨城県なんとか市');
 
+
 -- +migrate Down
 DELETE FROM users;
+DELETE FROM items;
