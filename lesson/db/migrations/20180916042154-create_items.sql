931ab2da706831415172bb622fdbb08db58b451d
kobasato34 <weakest34@gmail.com>
2018-09-16 13:47:40 +0900

    ファイル名修正、SQL文書いた

--- lesson/db/migrations/20180916042154-create_items.sql
+++ lesson/db/migrations/20180916042154-create_items.sql
@@ -1,4 +1,10 @@
 
 -- +migrate Up
-
+CREATE TABLE items (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  price INT NOT NULL,
+  PRIMARY KEY(id)
+);
 -- +migrate Down
+DELETE FROM items;
