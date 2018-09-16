4d4b01a8ab07080833f921019ae978d2535dd0a4
yutsukimiyashita <j148015n@st.u-gakugei.ac.jp>
2018-09-16 15:49:12 +0900

    date change

--- modeling/db/migrations/20180916062433-create_order_history.sql
+++ modeling/db/migrations/20180916062433-create_order_history.sql
@@ -4,11 +4,14 @@ CREATE TABLE order_history (
   id INT NOT NULL AUTO_INCREMENT,
   user_id INT NOT NULL,
   vegetable_id INT NOT NULL,
-  date DATE DEFAULT CURRENT_DATE ON UPDATE CURRENT_DATE,
+  `date` DATE NOT NULL,
   PRIMARY KEY(id),
   FOREIGN KEY(user_id) REFERENCES users(id),
   FOREIGN KEY(vegetable_id) REFERENCES vegetables(id)
 );
 
 
+
+
 -- +migrate Down
+DROP TABLE order_history;
