621e52ffcd48786c58979fea8f0ef61982aaff8b
yutsukimiyashita <j148015n@st.u-gakugei.ac.jp>
2018-09-16 16:53:47 +0900

    [fix] pex

--- modeling/db/migrations/20180916074217-create_pex.sql
+++ modeling/db/migrations/20180916074217-create_pex.sql
@@ -3,7 +3,7 @@ CREATE TABLE pex (
   pex_id VARCHAR(255) NOT NULL,
   user_id INT NOT NULL,
   PRIMARY KEY(pex_id),
-  FOREIGN KEY(user_id) REFERENCES users(id),
+  FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
 -- +migrate Down
