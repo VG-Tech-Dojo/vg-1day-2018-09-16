10e2a66285328536222db473e3bf570ac9f8c831
yaya1026 <yaya.w.1026@gmail.com>
2018-09-16 16:47:56 +0900

    外部サービス

--- modeling/db/migrations/20180916074243-create_external_service.sql
+++ modeling/db/migrations/20180916074243-create_external_service.sql
@@ -1,4 +1,10 @@
 
 -- +migrate Up
+CREATE TABLE external_service (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(25) NOT NULL,
+  PRIMARY KEY(id)
+);
 
 -- +migrate Down
+DROP TABLE external_service;
