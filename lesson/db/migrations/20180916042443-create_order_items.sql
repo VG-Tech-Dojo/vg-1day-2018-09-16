302e9ef60df875af883ffd26ab257804a4d96175
Jack <tanda.jin@itolab.nitech.ac.jp>
2018-09-16 14:19:50 +0900

    create all migrations

--- lesson/db/migrations/20180916042443-create_order_items.sql
+++ lesson/db/migrations/20180916042443-create_order_items.sql
@@ -1,4 +1,14 @@
 
 -- +migrate Up
+CREATE TABLE order_items (
+	id INT NOT NULL AUTO_INCREMENT,
+	order_id INT NOT NULL,
+	item_id INT NOT NULL,
+	amount INT DEFAULT 1,
+	PRIMARY KEY (id),
+	FOREIGN KEY (order_id) REFERENCES orders (id),
+	FOREIGN KEY (item_id) REFERENCES items (id)
+);
 
 -- +migrate Down
+DROP TABLE order_items;
