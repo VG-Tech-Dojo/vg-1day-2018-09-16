98a3dc74723f29d969f06467aa55be09bd626ff3
hamasugar <hamazi511@gmail.com>
2018-09-16 14:09:20 +0900

    third commit

--- db/migrations/20180916043746-create_items.sql
+++ db/migrations/20180916043746-create_items.sql
@@ -1,4 +1,14 @@
 
 -- +migrate Up
 
+CREATE TABLE items (
+  id INT NOT NULL AUTO_INCREMENT,
+  price INT NOT NULL ,
+  name VARCHAR(255) NOT NULL,
+  PRIMARY KEY(id)
+);
+
+
+
 -- +migrate Down
+DROP TABLE items;
